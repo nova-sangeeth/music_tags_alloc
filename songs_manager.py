51dc04f5f6f22dc5e9382c4a2b920368d8a023d7
nova-sangeeth <54772424+nova-sangeeth@users.noreply.github.com>
2019-10-23 15:15:02 +0530

    adding functions to split a song according to the year it was put out Splitting the commands for each functionalities (aka tags)

--- songs_manager.py
+++ songs_manager.py
@@ -1,10 +1,13 @@
+# This is a script used to split the songs according to the genre or artist oor even year
+
+
 from tinytag import TinyTag
 import shutil
 
 
 #  DISPLAY THE DETAILS OF THE FILE:
 def song_details():
-#   print('---------------------------------------------------')
+    #   print('---------------------------------------------------')
     print(f'Title: {song_file.title}')
     print(f'Artist name: {song_file.artist}')
     print(f'the album is: {song_file.album}')
@@ -13,23 +16,26 @@ def song_details():
     print(f'Bitrate: {song_file.bitrate}')
     print(f'Duration in Seconds: {song_file.duration}')
     print('---------------------------------------------------')
+
+
 # sending the song to the genre folder
 
-def sort_logic():
-    if song_file.genre =='R&B/Soul':
-        shutil.move(f'{song_path}',r'C:\Users\novas\PycharmProjects\music_tags_alloc\the songs data\point B\soul')
+def sort_logic_genre():
+    if song_file.genre == 'R&B/Soul':
+        shutil.move(f'{song_path}', r'C:\Users\novas\PycharmProjects\music_tags_alloc\the songs data\point B\soul')
         print('this song is moved to the soul folder')
-    elif song_file.genre =='Hip-Hop/Rap':
+    elif song_file.genre == 'Hip-Hop/Rap':
         shutil.move(f'{song_path}', r'C:\Users\novas\PycharmProjects\music_tags_alloc\the songs data\point B\Rap')
         print('this song is moved to the Rap folder')
-    elif song_file.genre =='Pop':
+    elif song_file.genre == 'Pop':
         shutil.move(f'{song_path}', r'C:\Users\novas\PycharmProjects\music_tags_alloc\the songs data\point B\Pop')
         print('this song is moved to the Pop folder')
-    elif song_file.genre =='Dance':
+    elif song_file.genre == 'Dance':
         shutil.move(f'{song_path}', r'C:\Users\novas\PycharmProjects\music_tags_alloc\the songs data\point B\dance')
         print('this song is moved to the Dance folder')
-    elif song_file.genre =='Alternative':
-        shutil.move(f'{song_path}', r'C:\Users\novas\PycharmProjects\music_tags_alloc\the songs data\point B\Alternatives')
+    elif song_file.genre == 'Alternative':
+        shutil.move(f'{song_path}',
+                    r'C:\Users\novas\PycharmProjects\music_tags_alloc\the songs data\point B\Alternatives')
     else:
         shutil.move(f'{song_path}', r'C:\Users\novas\PycharmProjects\music_tags_alloc\the songs data\point B\others')
         print("the song is moved to the other's folder ")
@@ -37,23 +43,48 @@ def sort_logic():
 
 # the sorting  of artists according to the music tags of the file
 def sort_artists():
-    if song_file.artist =='Lil Wayne':
-        shutil.move(f'{song_path}',r' THE REQUIRED DIR')
+    if song_file.artist == 'Lil Wayne':
+        shutil.move(f'{song_path}', r' THE REQUIRED DIR')
     elif song_file.artist == 'Drake':
-        shutil.move(f'{song_path}',r' THE REQUIRED DIRECTORY')
+        shutil.move(f'{song_path}', r' THE REQUIRED DIRECTORY')
     else:
-        shutil.move(f'{song_path}',r'The directory with the other folder ')
+        shutil.move(f'{song_path}', r'The directory with the other folder ')
 
-#LOOP TO GET THE PATH OF THE MUSIC FILE FROM THE USER.
+
+def sort_artist_year():
+    if song_file.year == '2019':
+        shutil.move(f'{song_path}', r'the directory for year folder ')
+    else:
+        shutil.move(f'{song_path}', r'the directory to other folders')
+
+
+# LOOP TO GET THE PATH OF THE MUSIC FILE FROM THE USER.
 
 
 song_path = input('enter the command')
-if song_path =='enter':
+if song_path == 'DETAILS':
     for path in song_path:
         song_path = input(r'Enter the path here: ')
         song_file = TinyTag.get(song_path)
         song_details()
-        sort_logic()
-        sort_artists()
-# the above created functions being called.
 
+# the loop for sorting the songs according to GENRE
+if song_path == 'GENRE':
+    for path in song_path:
+        song_path = input(r'Enter the path here: ')
+        song_file = TinyTag.get(song_path)
+        sort_logic_genre()
+
+# the loop for sorting the songs according to the Artists
+if song_path == 'ARTISTS':
+    for path in song_path:
+        song_path = input(r'Enter the path here: ')
+        song_file = TinyTag.get(song_path)
+        sort_artists()
+
+# the loop for sorting the songs according to the year
+if song_path == 'YEAR':
+    for path in song_path:
+        song_path = input(r'Enter the path here: ')
+        song_file = TinyTag.get(song_path)
+        sort_artist_year()
